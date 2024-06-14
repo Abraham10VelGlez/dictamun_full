a8a05cf2b3636261122ef349fad2790480d939c3
Abraham10VelGlez <45047578+Abraham10VelGlez@users.noreply.github.com>
2024-06-14 12:39:42 -0600

    Update bd.php

--- bd/bd.php
+++ bd/bd.php
@@ -1,5 +1,4 @@
 <?php
-//$xxx = pg_connect("host=10.10.68.31 dbname=ctv user=postgres password='j7cr3a'") or die('No se ha podido conectar: ' . pg_last_error());
 $xxx = pg_connect("host=localhost dbname=ctv2022 user=postgres password='12345'") or die('No se ha podido conectar: ' . pg_last_error());
 //$xxx = pg_connect("host=localhost dbname=ctv2021 user=postgres password='12345'") or die('No se ha podido conectar: ' . pg_last_error());
 ?>
